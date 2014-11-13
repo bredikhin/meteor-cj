beb2845b24af9c8d992bf619f2d674f8a48a9cc9
Ruslan Bredikhin <rus.brn@gmail.com>
2014-11-12 20:36:01 -0500

    fix the tests

--- cj_tests.js
+++ cj_tests.js
@@ -7,13 +7,14 @@ var config = {
 // Test instance
 var cj = new Cj(config);
 
+// Options passed to the get request stub
+var optionsPassed;
+
 // GET request stubbed
 HTTP = {
   get: function(url, options, cb) {
-    var res = {
-      stubbed: true,
-      optionsPassed: options
-    };
+    optionsPassed = options;
+    var res = {};
     switch(url) {
       case cj.endpoints.productSearch:
         res.content = '<?xml version="1.0" encoding="UTF-8"?><cj-api>'
@@ -29,6 +30,7 @@ HTTP = {
         throw new Error('Invalid endpoint');
         break;
     }
+
     cb(null, res);
   }
 };
@@ -53,58 +55,68 @@ Tinytest.add('Cj - constructor - fails on invalid configuration', function (test
 });
 
 Tinytest.add('Cj - productSearch - makes a GET request', function (test) {
+  optionsPassed = null;
   cj.productSearch({}, function(err, res) {
     test.equal(err, null);
-    test.isTrue(res.stubbed);
+    test.include(optionsPassed, 'params');
+    test.include(optionsPassed, 'headers');
   });
 });
 
 Tinytest.add('Cj - productSearch - passes the parameters', function (test) {
+  optionsPassed = null;
   cj.productSearch({something: 'anything'}, function(err, res) {
-    test.include(res.optionsPassed.params, 'something');
-    test.equal(res.optionsPassed.params.something, 'anything');
+    test.include(optionsPassed.params, 'something');
+    test.equal(optionsPassed.params.something, 'anything');
   });
 });
 
 Tinytest.add('Cj - productSearch - sets the authorization header', function (test) {
+  optionsPassed = null;
   cj.productSearch({}, function(err, res) {
-    test.include(res.optionsPassed.headers, 'authorization');
-    test.equal(res.optionsPassed.headers.authorization, cj.developerKey);
+    test.include(optionsPassed.headers, 'Authorization');
+    test.equal(optionsPassed.headers['Authorization'], cj.developerKey);
   });
 });
 
 Tinytest.add('Cj - productSearch - parses the XML response to JSON', function (test) {
+  optionsPassed = null;
   cj.productSearch({}, function(err, res) {
-    test.instanceOf(res['$'], Array);
+    test.instanceOf(res['$'], Object);
     test.instanceOf(res.product, Array);
     test.length(res.product, 2);
   });
 });
 
 Tinytest.add('Cj - linkSearch - makes a GET request', function (test) {
+  optionsPassed = null;
   cj.productSearch({}, function(err, res) {
     test.equal(err, null);
-    test.isTrue(res.stubbed);
+    test.include(optionsPassed, 'params');
+    test.include(optionsPassed, 'headers');
   });
 });
 
 Tinytest.add('Cj - linkSearch - passes the parameters', function (test) {
+  optionsPassed = null;
   cj.linkSearch({something: 'anything'}, function(err, res) {
-    test.include(res.optionsPassed.params, 'something');
-    test.equal(res.optionsPassed.params.something, 'anything');
+    test.include(optionsPassed.params, 'something');
+    test.equal(optionsPassed.params.something, 'anything');
   });
 });
 
 Tinytest.add('Cj - linkSearch - sets the authorization header', function (test) {
+  optionsPassed = null;
   cj.linkSearch({}, function(err, res) {
-    test.include(res.optionsPassed.headers, 'authorization');
-    test.equal(res.optionsPassed.headers.authorization, cj.developerKey);
+    test.include(optionsPassed.headers, 'Authorization');
+    test.equal(optionsPassed.headers['Authorization'], cj.developerKey);
   });
 });
 
 Tinytest.add('Cj - linkSearch - parses the XML response to JSON', function (test) {
+  optionsPassed = null;
   cj.linkSearch({}, function(err, res) {
-    test.instanceOf(res['$'], Array);
+    test.instanceOf(res['$'], Object);
     test.instanceOf(res.link, Array);
     test.length(res.link, 2);
   });
