8ed99f0647796b7e0b53255c5ec4898d1a64778a
Ruslan Bredikhin <rus.brn@gmail.com>
2014-11-12 20:36:01 -0500

    fix testing, bump the version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Commission Junction (CJ Affiliate by Conversant) API wrapper for Meteor",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/bredikhin/meteor-cj"
 });
 
@@ -16,7 +16,9 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('bredikhin:cj');
-  api.addFiles('cj_tests.js');
+  api.versionsFrom('METEOR@1.0');
+  api.use(['tinytest', 'check'], 'server');
+  Npm.depends({xml2js: "0.4.4"});
+  api.addFiles('cj.js', 'server');
+  api.addFiles('cj_tests.js', 'server');
 });
